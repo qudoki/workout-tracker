96e13357e7f07299827b6aa34e1506b0935e808b
Qian Dong <qdong327@gmail.com>
2020-12-05 12:03:44 -0500

    last workout showing

--- public/workout.js
+++ public/workout.js
@@ -21,11 +21,11 @@ async function initWorkout() {
 
 function tallyExercises(exercises) {
   const tallied = exercises.reduce((acc, curr) => {
-    if (curr.type === "resistance") {
+    if (curr.type == "Resistance") {
       acc.totalWeight = (acc.totalWeight || 0) + curr.weight;
       acc.totalSets = (acc.totalSets || 0) + curr.sets;
       acc.totalReps = (acc.totalReps || 0) + curr.reps;
-    } else if (curr.type === "cardio") {
+    } else if (curr.type == "Cardio") {
       acc.totalDistance = (acc.totalDistance || 0) + curr.distance;
     }
     return acc;
