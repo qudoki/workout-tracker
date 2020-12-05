f949d3f252ef5c4f9d6de89c5fd6479551b34430
Qian Dong <qdong327@gmail.com>
2020-12-05 11:24:16 -0500

    fixed routes in api.js

--- routes/api.js
+++ routes/api.js
@@ -1,34 +1,6 @@
 const router = require("express").Router();
 const Workout = require("../models/workout");
 
-router.post("/api/workouts", (req, res) => {
-    Workout.create({})
-        .then(dbWorkout => {
-            res.json(dbWorkout);
-        })
-        .catch(err => {
-            res.json(err);
-        });
-});
-//logging below is now working
-router.put("/api/workouts/:id", ({ body, params }, res) => {
-// router.put("/api/workouts/: id", (req, res) => {
-    console.log("body");
-    // Below console.log returns { body: undefined, params: { id: '5fcb9bf4d43c85ddef97d823'}}
-    console.log({ body, params } );
-    Workout.findByIdAndUpdate(
-        params.id,
-        { $push: { exercises: body } },
-        { new: true, runValidators: true }
-    )
-    .then(dbWorkout => {
-        res.json(dbWorkout);
-    })
-    .catch(err => {
-        res.json(err);
-    })
-});
-
 router.get("/api/workouts", (req, res) => {
     Workout.find()
         .then(dbWorkouts => {
@@ -39,8 +11,39 @@ router.get("/api/workouts", (req, res) => {
         });
 });
 
+//logging below is now working
+// router.put("/api/workouts/:id", ({ body, params }, res) => {
+    router.put("/api/workouts/: id", (req, res) => {
+        console.log("body");
+        // Below console.log returns { body: undefined, params: { id: '5fcb9bf4d43c85ddef97d823'}}
+        // console.log({ body, params } );
+        Workout.findByIdAndUpdate(
+            req.params.id,
+            { $push: { exercises: body } },
+            { new: true, runValidators: true }
+        )
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        })
+    });
+
+router.post("/api/workouts", (req, res) => {
+    Workout.create({
+        day: Date.now()
+    })
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
 router.get("/api/workouts/range", (req, res) => {
-    Workout.find({}).limit(7)
+    Workout.find({}).limit(10)
         .then(dbWorkouts => {
             res.json(dbWorkouts);
         })
