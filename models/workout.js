ac446267da0af6b18899afa155a32c710967ec33
Qian Dong <qdong327@gmail.com>
2020-12-05 11:02:58 -0500

    added missing exercises file model

--- models/workout.js
+++ models/workout.js
@@ -4,7 +4,8 @@ const workoutSchema = new Schema(
     {
         day: {
             type: Date,
-            default: () => new Date()
+            // default: () => new Date()
+            required: true
         },
         exercises: [
             {
@@ -44,11 +45,11 @@ const workoutSchema = new Schema(
     }
 );
 
-workoutSchema.virtual("totalDuration").get(function () {
-    return this.exercises.reduce((total, exercise) => {
-        return total + exercise.duration;
-    }, 0);
-});
+// workoutSchema.virtual("totalDuration").get(function () {
+//     return this.exercises.reduce((total, exercise) => {
+//         return total + exercise.duration;
+//     }, 0);
+// });
 
 const Workout = mongoose.model("Workout", workoutSchema);
 
